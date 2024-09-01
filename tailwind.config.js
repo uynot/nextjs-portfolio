1f539dafe912015fd0c713503b9e64242d2432b6
uy <kaho399@gmail.com>
2024-09-01 03:39:43 -0400

    redesigned exp and edu section design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+			},
+			keyframes: {
+				pulse: {
+					"0%, 100%": { opacity: "0.5" },
+					"50%": { opacity: "0.25" },
+				},
+			},
+			backgroundImage: {
+				"dot-fade": "radial-gradient(circle, rgba(253, 224, 71, 1) 30%, rgba(253, 224, 71, 0) 70%)",
+			},
+		},
 	},
 	plugins: [require("tailwind-scrollbar"), require("daisyui")],
 };
